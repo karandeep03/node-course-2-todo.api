fbcc4436684db9d5d7492cce38eaacc2c30066e9
Karandeep <karandeepsingh47@gmail.com>
2019-01-28 18:26:35 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,31 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GEt /todos/123445
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+   if(!ObjectID.isValid(id)){
+     res.status(404).send();
+   }
+
+   // findById
+    // success
+     // if todo - send it back
+     // if no todo - send back 404 with empty body
+    // error
+     // 400 - and send empty body back
+     Todo.findById(id).then((todo) => {
+       if(!todo) {
+         return res.status(404).send();
+       }
+       res.send({todo});
+       console.log('Todo By Id', todo);
+     }).catch((e) => {
+       res.status(400).send()
+     });
 });
 
 app.listen(3000, () => {
